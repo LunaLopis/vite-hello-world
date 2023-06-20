e71711165ad6ef215de3b0bd4288447ae6993b36
Luna Lopis <lunalopis59@gmail.com>
2023-06-20 18:11:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components:{
+    AppTitle
+  }
+}
+</script>
+
+
+<template >
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+
+<style lang="">
+  
+</style>
